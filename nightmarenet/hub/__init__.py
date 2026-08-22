426ed52f083788fe4cc520e28119867dc2ae88e7
mercael <mercael91@gmail.com>
2026-08-22 09:01:44 +0300

    fix: import PyTorchModelHubMixin from huggingface_hub; return commit URL from push_to_hub

--- nightmarenet/hub/__init__.py
+++ nightmarenet/hub/__init__.py
@@ -1,8 +1,9 @@
-from transformers import PyTorchModelHubMixin
+from huggingface_hub import PyTorchModelHubMixin
+
 
 class NightmareNetHubMixin(PyTorchModelHubMixin):
     def push_to_hub(self, repo_id, **kwargs):
-        super().push_to_hub(repo_id, **kwargs)
+        return super().push_to_hub(repo_id, **kwargs)
 
     @classmethod
     def from_pretrained(cls, pretrained_model_name_or_path, **kwargs):
